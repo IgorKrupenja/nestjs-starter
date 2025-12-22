c4079fe7ca27093f38ee029dbfccb579ccb99319
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-22 15:04:44 +0200

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, Logger, LogLevel, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import compression from 'compression';
@@ -7,7 +7,17 @@ import { AppModule } from './app.module.js';
 import { PrismaExceptionFilter } from './prisma/filters/prisma-exception.filter.js';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  const logger = new ConsoleLogger({
+    logLevels: (process.env.LOGGER_LOG_LEVELS?.split(',') as LogLevel[]) || [
+      'error',
+      'warn',
+      'log',
+    ],
+    timestamp: true,
+    colors: process.env.LOGGER_COLORS === 'true',
+  });
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true, logger });
   app.use(compression());
   // Validate DTOs for incoming requests globally
   // Also rejects requests with non-whitelisted properties
