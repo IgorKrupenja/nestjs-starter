8f8ee830e5de8225f562120c6686a4f7805b2762
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-26 13:53:35 +0200

    tupes

--- src/common/interceptors/request-logger.interceptor.ts
+++ src/common/interceptors/request-logger.interceptor.ts
@@ -1,28 +1,18 @@
 import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs';
-
-interface RequestData {
-  originalUrl: string;
-  method: string;
-  params: Record<string, unknown>;
-  query: Record<string, unknown>;
-  body: Record<string, unknown>;
-}
-
-interface ResponseData {
-  statusCode: number;
-}
 
 @Injectable()
 export class RequestLogger implements NestInterceptor {
   private readonly logger = new Logger(RequestLogger.name);
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-    const request = context.switchToHttp().getRequest<RequestData>();
-    const response = context.switchToHttp().getResponse<ResponseData>();
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
 
-    const { originalUrl, method, params, query, body } = request;
+    const { originalUrl, method, params, query } = request;
+    const body = request.body as Record<string, unknown>;
     const { statusCode } = response;
 
     return next.handle().pipe(
