9243f446ef157c2d677711e320a695479c20c8cb
Igor Krupenja <igor.krupenja@gmail.com>
2026-01-24 13:02:12 +0200

    fix e2e

--- src/post/dtos/post.dto.ts
+++ src/post/dtos/post.dto.ts
@@ -1,34 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { PostModel } from '@src/generated/prisma/models';
+import { Expose } from 'class-transformer';
 
 export class PostDto implements PostModel {
   @ApiProperty({
     description: 'The unique identifier of the post.',
     example: 1,
   })
+  @Expose()
   id!: number;
 
   @ApiProperty({
     description: 'The title of the post.',
     example: 'On programming languages',
   })
+  @Expose()
   title!: string;
 
   @ApiProperty({
     description: 'The content of the post.',
     example: 'TypeScript is the best programming language.',
   })
+  @Expose()
   content!: string;
 
   @ApiProperty({
     description: 'Whether the post is published or not.',
     example: true,
   })
+  @Expose()
   published!: boolean;
 
   @ApiProperty({
     description: 'The unique identifier of the author of the post.',
     example: 1,
   })
+  @Expose()
   authorId!: number;
 }
